46b6b98206726111f44ca65ee765daeab040aaaf
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-08-19 12:06:42 +0900

    Fix for migrate-file

--- db/migrate/20170819024827_create_groups.rb
+++ db/migrate/20170819024827_create_groups.rb
@@ -1,8 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string :group_id
-      t.integer :group_name
+      t.integer :name
 
       t.timestamps
     end
