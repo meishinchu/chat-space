ad87229c92685ba0037035a2cedce14836e75ef8
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-08-21 21:14:13 +0900

    Add group create action and add flashmesseage

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,17 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    if Group.create(group_params)
+      flash.now[:notice] = "グループを作成しました"
+      render "messages/index.html.haml"
+    else
+      flash.now[:alert] = "グループ名を入力してください"
+      render :new
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 end
