9d7169da8e2e2d9c81b8f3fa48f3849f9c9c56c1
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-09-02 20:21:29 +0900

    Add create action in routes.rb, messages_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root  'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, except: [:show, :destroy] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
 end
