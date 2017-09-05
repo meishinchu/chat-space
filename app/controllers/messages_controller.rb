2bf92bafdf0050cea7b0a885e683de9c3db35dca
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-09-06 00:17:46 +0900

    Add the action to save the message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,18 @@
 class MessagesController < ApplicationController
 
   def index
+    @group = Group.find(params[:group_id])
+    @message = @group.messages.build
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to group_messages_path
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
 end
