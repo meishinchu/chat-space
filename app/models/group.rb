cfbf9863fc3e2795a9230b76d51b9bf3cde00d91
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-09-09 19:33:49 +0900

    Fix to define the latest message display in the group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,12 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
 
   validates :name, presence: true
+
+  def get_lastest_message
+    if self.messages.blank?
+      "まだメッセージはありません。"
+    else
+      self.messages.last.text
+    end
+  end
 end
