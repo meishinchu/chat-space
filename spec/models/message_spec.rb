45736606f96d168ad751d93e91698baeab383954
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-09-16 19:35:22 +0900

    Make test for "is invalid without a text and image"

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,5 +18,11 @@ describe Message do
       expect(message).to be_valid
     end
 
+    it "is invalid without a text and image" do
+      message = build(:message, text: "", image: "")
+      message.valid?
+      expect(message.errors[:text_or_image][0]).to include("入力してください")
+    end
+
   end
 end
