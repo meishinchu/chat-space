a87365ffee0b8705fbba03bf1f6e9c1dc18127fd
meishinchu <i-shinchu-mei@hotmail.co.jp>
2017-09-23 14:42:04 +0900

    Add the processing of asynchronous communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,37 @@
-$(function(){ 
+$(function(){
+  function buildHTML(message){
+    if(message.image){
+      var html = `<div class ="chat-main__message">
+                    <p class ="chat-main__message__member">
+                      ${message.name}
+                    </p>
+                    <p class ="chat-main__message__day">
+                      ${message.created_at}
+                    </p>
+                  <div class ="chat-main__text">
+                    <p class ="chat-main__text__word">
+                      ${message.text}
+                    </p>
+                      <img src="${message.image}" class ="chat-main__image">`
+    }else{
+      var html = `<div class ="chat-main__message">
+                    <p class ="chat-main__message__member">
+                      ${message.name}
+                    </p>
+                    <p class ="chat-main__message__day">
+                      ${message.created_at}
+                    </p>
+                  <div class ="chat-main__text">
+                    <p class ="chat-main__text__word">
+                      ${message.text}
+                    </p>`
+    }
+    return html;
+  }
   $('#new_message').on('submit', function(e){ 
     e.preventDefault(); 
     var formData = new FormData(this); 
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,5 +39,16 @@ $(function(){ 
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log(html);
+      $('.chat-main').append(html);
+      $('.chat-form__input__text').val('');
+      $('.chat-main').animate({scrollTop:$('.chat-main')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
- })
+ });
